231fed1af057a5c434218f25c2228ffa3f46b8b0
mjescalante <122808004+mjescalante@users.noreply.github.com>
2024-01-29 09:24:45 +0100

    Proyecto Inicial
    Proyecto de inicio

---
+++ T1_P1/target/generated-sources/annotations/iestr/jmff/t1_p1/modelo/Tablapapeleria_.java
@@ -0,0 +1,17 @@
+package iestr.jmff.t1_p1.modelo;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-25T18:05:46", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Tablapapeleria.class)
+public class Tablapapeleria_ { 
+
+    public static volatile SingularAttribute<Tablapapeleria, String> codigo;
+    public static volatile SingularAttribute<Tablapapeleria, Double> precio;
+    public static volatile SingularAttribute<Tablapapeleria, Integer> id;
+    public static volatile SingularAttribute<Tablapapeleria, String> producto;
+    public static volatile SingularAttribute<Tablapapeleria, String> familia;
+
+}
